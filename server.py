adbd2d590d111769d05a35e8ed343c8f42828e4b
Piolo Torrecampo <94138318+papz-i@users.noreply.github.com>
2024-06-06 22:32:20 +0800

    revised

--- server.py
+++ server.py
@@ -1,40 +1,93 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 import json
 
 app = Flask(__name__)
 
-@app.route("/rotate")
-def rotate():
-    file = open (r"data\rotate.json", "r")
-    data = json.loads(file.read())
-    return data
+currentInstruction = {
+    "prompt": "",
+    "action": "",
+    "parameters": ""
+}
 
-@app.route("/scale")
-def scale():
-    file = open (r"data\scale.json", "r")
-    data = json.loads(file.read())
-    return data
+errorResponse = {
+    "message": ""
+}
 
-@app.route("/remove")
-def remove():
-    file = open (r"data\remove.json", "r")
-    data = json.loads(file.read())
-    return data
+actions = ['insert', 'duplicate', 'delete', 'move', 'replace', 'rotate', 'scale']
+insert_params = ['reference_object', 'prefab', 'direction', 'value']
+duplicate_params = ['prefab', 'axis']
+move_params = ['prefab', 'direction', 'value']
+remove_params = ['prefab', 'value']
+replace_params = ['prefab', 'object_to_replace']
+rotate_params = ['prefab', 'axis', 'value']
+scale_params = ['prefab', 'axis', 'value']
 
-@app.route("/replace")
-def replace():
-    file = open (r"data\replace.json", "r")
-    data = json.loads(file.read())
-    return data
+def validateParameters(parameterKeys, parameterValues):
+    checkParameters = True
+    missingParams = []
+    for item in parameterValues:
+        if item not in parameterKeys:
+            missingParams.append(item)
+            checkParameters = False
+    return checkParameters, missingParams
 
-@app.route("/move")
-def move():
-    file = open (r"data\move.json", "r")
-    data = json.loads(file.read())
-    return data
+@app.route("/set/instruction", methods=["POST"])
+def set_instruction(): 
+    action = request.json.get("action")
+    prompt = request.json.get("prompt")
+    parameters = request.json.get("parameters")
+    parameterKeys = list(parameters.keys())
 
-@app.route("/insert")
-def insert():
-    file = open (r"data\insert.json", "r")
-    data = json.loads(file.read())
-    return data
+    isAction = True
+    
+    match action:
+        case 'insert':
+            checkParameters, missingParams = validateParameters(parameterKeys, insert_params)
+            if(checkParameters == False):
+                errorResponse['message'] = f"Incompatible parameters. {str(missingParams)}"
+        case 'duplicate':
+            checkParameters, missingParams = validateParameters(parameterKeys, duplicate_params)
+            if(checkParameters == False):
+                errorResponse['message'] = f"Incompatible parameters. {str(missingParams)}"
+        case 'move':
+            checkParameters, missingParams = validateParameters(parameterKeys, move_params)
+            if(checkParameters == False):
+                errorResponse['message'] = f"Incompatible parameters. {str(missingParams)}"
+        case 'remove':
+            checkParameters, missingParams = validateParameters(parameterKeys, remove_params)
+            if(checkParameters == False):
+                errorResponse['message'] = f"Incompatible parameters. {str(missingParams)}"
+        case 'replace':
+            checkParameters, missingParams = validateParameters(parameterKeys, replace_params)
+            if(checkParameters == False):
+                errorResponse['message'] = f"Incompatible parameters. {str(missingParams)}"
+        case 'rotate':
+            checkParameters, missingParams = validateParameters(parameterKeys, rotate_params)
+            if(checkParameters == False):
+                errorResponse['message'] = f"Incompatible parameters. {str(missingParams)}"
+        case 'scale':
+            checkParameters, missingParams = validateParameters(parameterKeys, missingParams)
+            if(checkParameters == False):
+                errorResponse['message'] = f"Incompatible parameters. {str(missingParams)}" 
+        case _:
+            isAction = False
+            errorResponse['message'] = f"Action named '{action}' is not supported."
+
+    if(isAction and checkParameters):
+        if(prompt == None or action == None or parameters == None):
+            errorResponse['message'] = "Incomplete variables."
+            return jsonify(errorResponse)
+        else:
+            currentInstruction["prompt"] = prompt
+            currentInstruction["action"] = action
+            currentInstruction["parameters"] = parameters
+            return jsonify(currentInstruction)
+    else:
+        return jsonify(errorResponse)
+
+@app.route("/get/instruction")
+def get_instruction():
+    return jsonify(currentInstruction)
+
+if __name__ == "__main__":
+    app.run(debug=True, port=9001)
